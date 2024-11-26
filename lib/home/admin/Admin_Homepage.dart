1bfcdedaab6bc9d063ca6e75acb2fe14ea359509
Captain-Yami <missionproject69@gmail.com>
2024-11-26 13:51:08 +0530

    guide text box created

--- lib/home/admin/Admin_Homepage.dart
+++ lib/home/admin/Admin_Homepage.dart
@@ -12,32 +12,25 @@ class _AdminHomepageState extends State<AdminHomepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:  const Color.fromARGB(255, 70, 182, 235),
+        backgroundColor: const Color.fromARGB(255, 70, 182, 235),
         title: Text('Admin Homepage'),
       ),
-             body: Column(
-        children: [
-          SizedBox(height: 50), // Add space of 50 pixels from the top
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            decoration: BoxDecoration(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10), // Add padding to the left
-              child: Text(
-                'Users',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text('User'
                 ),
               ),
+            SizedBox(width: 10), // Space between the text fields
+            Expanded(
+              child: Text('Guide')
+                ),
+              ]),
             ),
-          ),
-        ],
-      ),
-    );
+          
+        )
+      ;
   }
 }
